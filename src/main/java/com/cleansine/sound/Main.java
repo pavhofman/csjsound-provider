3ddd39c451adf013328318530f61c719330597c8
Pavel Hofman <pavhofman@gmail.com>
2022-08-07 11:54:04 +0200

    Test code - updates, various tests.

--- src/main/java/com/cleansine/sound/Main.java
+++ src/main/java/com/cleansine/sound/Main.java
@@ -1,5 +1,12 @@
 package com.cleansine.sound;
 
+import com.cleansine.sound.provider.SimpleMixer;
+import com.cleansine.sound.provider.SimpleMixerInfo;
+import com.cleansine.sound.provider.SimpleMixerProvider;
+
+import javax.sound.sampled.AudioFormat;
+import java.util.Vector;
+
 public class Main {
     /**
      * Example usage in {@link TestPlayerS16LE}
@@ -7,7 +14,36 @@ public class Main {
     public static void main(String[] args) {
         // test playback
         TestPlayerS16LE player = new TestPlayerS16LE(48000, 2);
-        player.play((info) -> !info.getName().contains("plughw") && info.getName().contains("itec"), TestPlayerS16LE.Op.LOOPBACK, 3000);
+        int cnt = SimpleMixerProvider.nGetMixerCnt();
+        for (int i = 0; i < cnt; i++) {
+            SimpleMixerInfo mixerInfo = SimpleMixerProvider.nCreateMixerInfo(i);
+            String deviceID = mixerInfo.getDeviceID();
+            deviceID = "";
+            continue;
+        }
+
+//        Vector<AudioFormat> deviceFormats = new Vector<>();
+//        SimpleMixer.nGetFormats("itec", true, deviceFormats);
+//        deviceFormats.clear();
+
+        Vector<AudioFormat> deviceFormats = new Vector<>();
+        SimpleMixer.nGetFormats("0", true, deviceFormats);
+        deviceFormats.clear();
+        SimpleMixer.nGetFormats("1", true, deviceFormats);
+
+        player.play(
+                (info) -> info.getName().startsWith("EXCL: Repro"),
+                //(info) -> info.getName().startsWith("EXCL: Micro"),
+                null,
+                TestPlayerS16LE.Op.SINE_1K, 3000
+        );
+
+//        player.play(
+//                info -> !info.getName().contains("plughw") && info.getName().contains("itec"),
+//                info -> !info.getName().contains("plughw") && info.getName().contains("itec"),
+//                //null,
+//                TestPlayerS16LE.Op.LOOPBACK, 3000
+//        );
         System.exit(0);
     }
 
--- src/main/java/com/cleansine/sound/TestPlayerS16LE.java
+++ src/main/java/com/cleansine/sound/TestPlayerS16LE.java
@@ -3,6 +3,8 @@ package com.cleansine.sound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.sound.sampled.*;
 import java.util.Arrays;
 import java.util.Optional;
@@ -12,40 +14,56 @@ import java.util.Optional;
  */
 public class TestPlayerS16LE {
     private static final Logger logger = LoggerFactory.getLogger(TestPlayerS16LE.class);
+    public static final int BUFFER_BYTES = 131072;
     private final int samplerate;
     private final int sampleBits;
     private final int channels;
 
     public TestPlayerS16LE(int samplerate, int channels) {
         this.samplerate = samplerate;
-        this.sampleBits = 16;
+        this.sampleBits = 32;
         this.channels = channels;
     }
 
 
-    public void play(Selector selector, Op what, int length_ms) {
+    public void play(@Nonnull Selector sourceSelector, @Nullable Selector targetSelector, @Nonnull Op what, int length_ms) {
         final AudioFormat af = new AudioFormat(samplerate, sampleBits, channels, true, false);
         final DataLine.Info sLineInfo = new DataLine.Info(SourceDataLine.class, af);
-        final DataLine.Info tLineInfo = new DataLine.Info(TargetDataLine.class, new AudioFormat(samplerate, sampleBits, 2, true, false));
+        final DataLine.Info tLineInfo = new DataLine.Info(TargetDataLine.class, af);
         try {
             Mixer.Info[] infos = AudioSystem.getMixerInfo();
-            Optional<Mixer.Info> info = Arrays.stream(infos)
-                    .filter(i -> selector.filter(i) && !i.getName().contains("Port"))
+            Optional<Mixer.Info> sourceInfo = Arrays.stream(infos)
+                    .filter(i -> sourceSelector.filter(i) && !i.getName().contains("Port"))
                     .findFirst();
-            assert (info.isPresent());
-            SourceDataLine sLine = (SourceDataLine) AudioSystem.getMixer(info.get()).getLine(sLineInfo);
-            TargetDataLine tLine = (TargetDataLine) AudioSystem.getMixer(info.get()).getLine(tLineInfo);
-            sLine.open(af, this.samplerate);
-            tLine.open(af, this.samplerate);
-            tLine.start();
-
-            int bufferBytes = sLine.getBufferSize();
+            assert (sourceInfo.isPresent());
+            assert (sourceInfo.get() instanceof SourceDataLine);
+            Mixer sMixer = AudioSystem.getMixer(sourceInfo.get());
+//            DataLine.Info testInfo = new DataLine.Info(SourceDataLine.class, new AudioFormat(48000, 32, 2, true, false));
+//            boolean isSupported = sMixer.isLineSupported(testInfo);
+            SourceDataLine sLine = (SourceDataLine) sMixer.getLine(sLineInfo);
+            //AudioFormat naf = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 48000, 32, 2, 8, 48000, false);
+            sLine.open(af, BUFFER_BYTES);
+            TargetDataLine tLine = null;
+            int bufferBytes;
+            if (what.doLoopback) {
+                Optional<Mixer.Info> targetInfo = Arrays.stream(infos)
+                        .filter(i -> targetSelector.filter(i) && !i.getName().contains("Port"))
+                        .findFirst();
+                assert (targetInfo.isPresent());
+                assert (targetInfo.get() instanceof TargetDataLine);
+                tLine = (TargetDataLine) AudioSystem.getMixer(targetInfo.get()).getLine(tLineInfo);
+                tLine.open(af, BUFFER_BYTES);
+                bufferBytes = tLine.getBufferSize();
+                tLine.start();
+            } else
+                bufferBytes = sLine.getBufferSize();
             int frameBytes = af.getFrameSize();
-            int framesInWrite = bufferBytes / frameBytes;
+            int framesInWrite = 6144;
             double[] samples = what.getSamples(samplerate, length_ms);
 
             int toneIdx = 0;
-            boolean sLineStarted = false;
+            //sLine.start();
+
             while (toneIdx + framesInWrite < samples.length) {
                 byte[] buffer;
                 if (what.doLoopback) {
@@ -54,17 +72,16 @@ public class TestPlayerS16LE {
                 } else {
                     buffer = convertToBytes(samples, toneIdx, framesInWrite, frameBytes);
                 }
+                logger.debug("Playback before writing: availbytes " + sLine.available());
                 int countWritten = sLine.write(buffer, 0, buffer.length);
-                if (!sLineStarted) {
-                    sLine.start();
-                    sLineStarted = true;
-                }
+                logger.debug("Playback after writing: availbytes " + sLine.available());
 
                 toneIdx += framesInWrite;
             }
 
             sLine.close();
-            tLine.close();
+            if (tLine != null)
+                tLine.close();
         } catch (LineUnavailableException e) {
             logger.error(e.getLocalizedMessage());
         }
@@ -74,12 +91,14 @@ public class TestPlayerS16LE {
         byte[] buffer = new byte[framesInWrite * frameBytes];
         int limit = toneIdx + framesInWrite;
         int bufferIdx = 0;
+        byte[] sampleBytes = new byte[4];
         for (int i = toneIdx; i < limit; ++i) {
             double sine = sines[i];
-            short sample = (short) (sine * Short.MAX_VALUE);
-            byte[] sampleBytes = new byte[2];
+            int sample = (int) (sine * Integer.MAX_VALUE);
             sampleBytes[0] = (byte) (sample & 0xff);
             sampleBytes[1] = (byte) ((sample >> 8) & 0xff);
+            sampleBytes[2] = (byte) ((sample >> 16) & 0xff);
+            sampleBytes[3] = (byte) ((sample >> 24) & 0xff);
 
             for (int ch = 0; ch < channels; ++ch) {
                 for (byte sampleByte : sampleBytes) {
